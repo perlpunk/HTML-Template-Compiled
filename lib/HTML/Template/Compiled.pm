f64c37ee0df5088fe5aed59899decb7bb847ced7
Tina Mueller <tinita@cpan.org>
2012-12-01 21:08:37 +0100

    remove H::T::C::Plugin::NumberFormat, version 1.000_002

--- lib/HTML/Template/Compiled.pm
+++ lib/HTML/Template/Compiled.pm
@@ -2,7 +2,7 @@ package HTML::Template::Compiled;
 # $Id: Compiled.pm 1161 2012-05-05 14:00:22Z tinita $
 # doesn't work with make tardist
 #our $VERSION = ($version_pod =~ m/^\$VERSION = "(\d+(?:\.\d+)+)"/m) ? $1 : "0.01";
-our $VERSION = "1.000_001";
+our $VERSION = "1.000_002";
 use Data::Dumper;
 use Scalar::Util;
 BEGIN {
@@ -1530,7 +1530,7 @@ HTML::Template::Compiled - Template System Compiles HTML::Template files to Perl
 
 =head1 VERSION
 
-$VERSION = "1.000_001"
+$VERSION = "1.000_002"
 
 =cut
 
